ca10a075ab4945b43e522822b3c06b15968a2c0a
Myles Forrest <lunaetic@lunaecy.com>
2020-12-04 17:45:06 -0500

    Stubbing out tests for Handlers, as they are not currently under test (unit)

---
+++ app/code/Magento/Catalog/Test/Unit/Model/Product/Gallery/UpdateHandlerTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Catalog\Test\Unit\Model\Product\Gallery;
+
+use PHPUnit\Framework\TestCase;
+
+class UpdateHandlerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+    }
+
+    public function testB()
+    {
+        $this->assertEquals(1, 0);
+    }
+}
