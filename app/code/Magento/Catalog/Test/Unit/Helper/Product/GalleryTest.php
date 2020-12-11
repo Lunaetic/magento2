62db1fd5a3e870965fc4a1684fdfff1c4678ebfe
Myles Forrest <lunaetic@lunaecy.com>
2020-12-11 18:14:02 -0500

    Add unit test for copyImage

--- app/code/Magento/Catalog/Test/Unit/Helper/Product/GalleryTest.php
+++ app/code/Magento/Catalog/Test/Unit/Helper/Product/GalleryTest.php
@@ -14,17 +14,20 @@ use Magento\Catalog\Model\ResourceModel\Product\Gallery as GalleryResource;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Filesystem\Directory\Write;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\MediaStorage\Helper\File\Storage\Database;
 use Magento\Store\Model\Store;
 use Magento\Store\Model\StoreManager;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use ReflectionException;
 
 /**
  */
 class GalleryTest extends TestCase
 {
     /**
-     * @var Gallery
+     * @var Gallery|MockObject
      */
     protected $subject;
 
@@ -49,19 +52,22 @@ class GalleryTest extends TestCase
     protected $storeManageMock;
 
     /**
+     * @var Database|MockObject
+     */
+    protected $fileStorageDbMock;
+
+    /**
      */
     public function setUp(): void
     {
-        $objectManager = new ObjectManager($this);
-
         $this->mediaConfigMock = $this->createPartialMock(
             Config::class,
-            ['getBaseMediaPath']
+            ['getBaseMediaPath', 'getMediaPath', 'getMediaShortUrl']
         );
 
         $this->mediaDirectoryMock = $this->createPartialMock(
             Write::class,
-            ['isFile']
+            ['isFile', 'getAbsolutePath', 'delete', 'copyFile']
         );
 
         $this->resourceModelMock = $this->createPartialMock(
@@ -74,13 +80,21 @@ class GalleryTest extends TestCase
             ['getStores']
         );
 
+        $this->fileStorageDbMock = $this->createPartialMock(
+            Database::class,
+            ['checkDbUsage', 'copyFile']
+        );
+
+        $objectManager = new ObjectManager($this);
+
         $this->subject = $objectManager->getObject(
             Gallery::class,
             [
                 'mediaConfig' => $this->mediaConfigMock,
                 'mediaDirectory' => $this->mediaDirectoryMock,
                 'resourceModel' => $this->resourceModelMock,
-                'storeManager' => $this->storeManageMock
+                'storeManager' => $this->storeManageMock,
+                'fileStorageDb' => $this->fileStorageDbMock
             ]
         );
     }
@@ -244,5 +258,156 @@ class GalleryTest extends TestCase
         $actual = $this->subject->canRemoveImage($productMock, 'test.jpg');
 
         $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @throws LocalizedException
+     * @throws ReflectionException
+     */
+    public function testCopyImageWithException(): void
+    {
+        $this->subject = $this->createPartialMock(
+            Gallery::class,
+            ['getUniqueFilename']
+        );
+
+        $this->setPropertyValues(
+            $this->subject,
+            [
+                'mediaConfig' => $this->mediaConfigMock,
+                'mediaDirectory' => $this->mediaDirectoryMock,
+                'resourceModel' => $this->resourceModelMock,
+                'storeManager' => $this->storeManageMock,
+                'fileStorageDb' => $this->fileStorageDbMock
+            ]
+        );
+
+        $this->subject->expects($this->once())
+            ->method('getUniqueFilename')
+            ->willReturn('test.jpg');
+
+        $this->mediaConfigMock->expects($this->exactly(2))
+            ->method('getMediaPath')
+            ->with('test.jpg')
+            ->willReturn('media/test.jpg');
+
+        $this->mediaDirectoryMock->expects($this->once())
+            ->method('isFile')
+            ->with('media/test.jpg')
+            ->willReturn(false);
+
+        $this->expectException(LocalizedException::class);
+
+        $this->subject->copyImage('test.jpg');
+    }
+
+    /**
+     * @return array
+     */
+    public function getCopyImageDataProvider(): array
+    {
+        return [
+            [
+                true,
+                2
+            ],
+            [
+                false,
+                3
+            ]
+        ];
+    }
+
+    /**
+     * @param $checkDbUsageReturn
+     * @param $getMediaPathTimes
+     * @throws LocalizedException
+     * @throws ReflectionException
+     *
+     * @dataProvider getCopyImageDataProvider
+     */
+    public function testCopyImage($checkDbUsageReturn, $getMediaPathTimes): void
+    {
+        $this->subject = $this->createPartialMock(
+            Gallery::class,
+            ['getUniqueFilename']
+        );
+
+        $this->setPropertyValues(
+            $this->subject,
+            [
+                'mediaConfig' => $this->mediaConfigMock,
+                'mediaDirectory' => $this->mediaDirectoryMock,
+                'resourceModel' => $this->resourceModelMock,
+                'storeManager' => $this->storeManageMock,
+                'fileStorageDb' => $this->fileStorageDbMock
+            ]
+        );
+
+        $this->subject->expects($this->once())
+            ->method('getUniqueFilename')
+            ->willReturn('test.jpg');
+
+        $this->mediaConfigMock->expects($this->exactly($getMediaPathTimes))
+            ->method('getMediaPath')
+            ->with('test.jpg')
+            ->willReturn('media/test.jpg');
+
+        $this->mediaDirectoryMock->expects($this->once())
+            ->method('isFile')
+            ->with('media/test.jpg')
+            ->willReturn(true);
+
+        $this->fileStorageDbMock->expects($this->once())
+            ->method('checkDbUsage')
+            ->willReturn($checkDbUsageReturn);
+
+        if ($checkDbUsageReturn) {
+            $this->fileStorageDbMock->expects($this->once())
+                ->method('copyFile')
+                ->with('absolute/media/test.jpg', 'media/test.jpg');
+
+            $this->mediaDirectoryMock->expects($this->once())
+                ->method('getAbsolutePath')
+                ->with('media/test.jpg')
+                ->willReturn('absolute/media/test.jpg');
+
+            $this->mediaConfigMock->expects($this->exactly(2))
+                ->method('getMediaShortUrl')
+                ->with('test.jpg')
+                ->willReturn('media/test.jpg');
+
+            $this->mediaDirectoryMock->expects($this->once())
+                ->method('delete')
+                ->with('media/test.jpg');
+
+        } else {
+            $this->mediaDirectoryMock->expects($this->once())
+                ->method('copyFile')
+                ->with('media/test.jpg', 'media/test.jpg');
+        }
+
+        $actual = $this->subject->copyImage('test.jpg');
+
+        $this->assertEquals($actual, 'test.jpg');
+    }
+
+    /**
+     * @param $object
+     * @param $propertyValuesArray
+     * @return object
+     * @throws ReflectionException
+     */
+    private function setPropertyValues($object, $propertyValuesArray): object
+    {
+        $reflection = new ReflectionClass(get_class($object));
+
+        foreach ($propertyValuesArray as $property => $value) {
+            $reflectionProperty = $reflection->getProperty($property);
+            $reflectionProperty->setAccessible(true);
+            $reflectionProperty->setValue($object, $value);
+        }
+
+        return $object;
     }
 }
